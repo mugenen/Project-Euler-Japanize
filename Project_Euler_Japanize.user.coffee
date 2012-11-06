7457a04c441e431cd61617f749eb1c545c8078ff
oga <8monkey.theorem@gmail.com>
2012-11-06 23:24:28 +0900

    Initial commit. version 1.0

---
+++ Project_Euler_Japanize.user.coffee
@@ -0,0 +1,38 @@
+'use strict';
+
+getProblemNumber = () ->
+    document.URL.split('=')[1].replace(/^0+/, '')
+
+getJapanized = (num) ->
+    xhr = $.ajax
+        url: "http://odz.sakura.ne.jp/projecteuler/index.php?Problem%20#{num}"
+        async: false
+        crossDomain: true
+    text = xhr.responseText
+    start = text.indexOf('<div id="body"')
+    end_outer = text.lastIndexOf('</table>')
+    end_inner = text.lastIndexOf('</div>', text.lastIndexOf('</table>'))
+    if start == -1 or end_outer == -1 or end_inner == -1
+        return
+    target = text.substring(start, end_inner + 6)
+    jp_wiki = $(target)
+    jp_wiki.children()
+
+getProblemName = (jp) ->
+    jp.eq(0).contents().eq(1).text()
+
+num = getProblemNumber()
+
+if not $.isNumeric(num)
+    return
+
+jp = getJapanized(num)
+if not jp?
+    return
+
+name = getProblemName(jp)
+
+$('h2').append(name)
+$('.problem_content').prepend($('<hr>'))
+$('.problem_content').prepend($("<p style=\"text-align: right;\"><a href=\"http://odz.sakura.ne.jp/projecteuler/index.php?Problem%20#{num}\">from http://odz.sakura.ne.jp/projecteuler/index.php?Problem%20#{num}</a></p>"))
+$('.problem_content').prepend(jp.slice(1))
---
+++ package/Project_Euler_Japanize.user.js
@@ -0,0 +1,48 @@
+(function() {
+  'use strict';
+  var getJapanized, getProblemName, getProblemNumber, jp, name, num;
+
+  getProblemNumber = function() {
+    return document.URL.split('=')[1].replace(/^0+/, '');
+  };
+
+  getJapanized = function(num) {
+    var end_inner, end_outer, jp_wiki, start, target, text, xhr;
+    xhr = $.ajax({
+      url: "http://odz.sakura.ne.jp/projecteuler/index.php?Problem%20" + num,
+      async: false,
+      crossDomain: true
+    });
+    text = xhr.responseText;
+    start = text.indexOf('<div id="body"');
+    end_outer = text.lastIndexOf('</table>');
+    end_inner = text.lastIndexOf('</div>', text.lastIndexOf('</table>'));
+    if (start === -1 || end_outer === -1 || end_inner === -1) return;
+    target = text.substring(start, end_inner + 6);
+    jp_wiki = $(target);
+    return jp_wiki.children();
+  };
+
+  getProblemName = function(jp) {
+    return jp.eq(0).contents().eq(1).text();
+  };
+
+  num = getProblemNumber();
+
+  if (!$.isNumeric(num)) return;
+
+  jp = getJapanized(num);
+
+  if (!(jp != null)) return;
+
+  name = getProblemName(jp);
+
+  $('h2').append(name);
+
+  $('.problem_content').prepend($('<hr>'));
+
+  $('.problem_content').prepend($("<p style=\"text-align: right;\"><a href=\"http://odz.sakura.ne.jp/projecteuler/index.php?Problem%20" + num + "\">from http://odz.sakura.ne.jp/projecteuler/index.php?Problem%20" + num + "</a></p>"));
+
+  $('.problem_content').prepend(jp.slice(1));
+
+}).call(this);
